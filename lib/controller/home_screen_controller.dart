15688b16270198a4206ab53c518c2913e2c35103
Adil Azhar <adil@tecspine.com>
2025-10-07 10:39:54 +0500

    Add HomeScreenController for managing logout functionality and state; update HomeScreen to utilize the controller.

---
+++ lib/controller/home_screen_controller.dart
@@ -0,0 +1,36 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:task_5_firebase_auth_firestore/data/auth_repository.dart';
+import 'package:task_5_firebase_auth_firestore/login_screen.dart';
+
+class HomeScreenController extends GetxController {
+  final AuthRepository authRepository;
+
+  final Rx<RxStatus> status = RxStatus.empty().obs;
+
+  HomeScreenController() : authRepository = AuthRepository();
+
+  void logout() async {
+    try {
+      status.value = RxStatus.loading();
+      Get.defaultDialog(
+        title: 'Logging Out',
+        content: const CircularProgressIndicator(),
+        barrierDismissible: false,
+      );
+      await authRepository.signOut();
+      status.value = RxStatus.success();
+      Get.back();
+      Get.snackbar('Success', 'Logout successful');
+      // Navigate to login screen
+      Get.off(() => const LoginScreen());
+    } catch (e) {
+      Get.back();
+      log('Error in logout: $e');
+      Get.snackbar('Error', 'Logout failed. ${e.toString()}');
+      status.value = RxStatus.error(e.toString());
+    }
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:task_5_firebase_auth_firestore/controller/home_screen_controller.dart';
 import 'package:task_5_firebase_auth_firestore/update_profile_widget.dart';
 import 'package:task_5_firebase_auth_firestore/utils/dimen.dart';
 
@@ -7,22 +9,38 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(HomeScreenController());
     return Scaffold(
       body: Padding(
         padding: EdgeInsetsGeometry.all(Dimen.s16),
         child: Center(
-          child: Card(
-            child: ListTile(
-              title: Text('Adil Azhar'),
-              subtitle: Text('adilazhar6015@gmail.com'),
-              trailing: Icon(Icons.email),
+          child: Column(
+            spacing: Dimen.s20,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Card(
+                child: ListTile(
+                  title: Text('Adil Azhar'),
+                  subtitle: Text('adilazhar6015@gmail.com'),
+                  trailing: Icon(Icons.email),
 
-              // tapping on the listitle opens up a dilaog with the name field to update the name of the person
-              onTap: () => showDialog(
-                context: context,
-                builder: (context) => UpdateProfileWidget('Adil Azhar'),
+                  // tapping on the listitle opens up a dilaog with the name field to update the name of the person
+                  onTap: () => showDialog(
+                    context: context,
+                    builder: (context) => UpdateProfileWidget('Adil Azhar'),
+                  ),
+                ),
               ),
-            ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    controller.logout();
+                  },
+                  child: const Text('Logout'),
+                ),
+              ),
+            ],
           ),
         ),
       ),
